34dce2bf85bb509826b8fc196c55bc17bf417bb4
nzlouis.com <nzlouis.com@gmail.com>
2025-09-01 02:13:29 +0000

    Fix Navbar issues.

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import Navbar from "@/components/Navbar";
 import CustomAIQuizPage from "./custom-ai-quiz/page";
 
 export default function Page() {
   return (
     <>
-      <Navbar />
       <CustomAIQuizPage />
     </>
   );
